8bc5e0645c88c58a22091465cb57faaff712a4fb
bestest-bugs <hcps-salihove2@henricostudents.org>
2025-03-26 09:52:51 -0400

    Add files via upload

--- thing.js
+++ thing.js
@@ -27,7 +27,6 @@ function spawnGato(sentId, buttonId, cost, numberSent) {
     }, 20000)
 
   } else if (cash < cost) {
-    var blink_speed = 250; // every 1000 == 1 second, adjust to suit
     var duration = 100
     let divHolder = document.querySelector('#buttonsHolder')
     console.log('#text' + numberSent, numberSent)
@@ -40,15 +39,61 @@ function spawnGato(sentId, buttonId, cost, numberSent) {
         if (executed < 3) {
             executed += 1;
             text.style.visibility = (text.style.visibility == 'hidden' ? '' : 'hidden');
-            //text.style.background = "linear-gradient(180deg, rgb(255, 0, 0), rgb(255, 0, 0))";
-            //console.log("bleugh")
-            //text.style.webkit-text-fill-color = "transparent";
-            //text.style.webkit-background-clip = "text";
-        } //else if (executed  < 4 && executed > 2) {
-          //executed = 5
-          //console.log("bleugh")
-         // text.style.background = "linear-gradient(180deg, rgb(253, 179, 7), rgb(244, 211, 101))"
-       // }
+        } 
+    };
+    //setInterval(blink, 1)
+    setInterval(blink, duration)
+    
+  }
+  return
+};
+
+
+
+function upgradeBank() {
+  console.log("hello!!")
+
+  //console.log("hi")
+  if (cash >= currentBankCost && level < 8) {
+    cash -= currentBankCost
+    currentBankCost += 560
+    level += 1
+    cashEarned += 1
+    cashCap += 1500
+    //console.log("h")
+    let button = document.querySelector('#bankButton')//.querySelector('#49')
+    let buttonLevel = button.querySelector('#bankLevel')
+    let buttonCost = button.querySelector('#bankCost')
+  
+    buttonLevel.innerHTML = "Level " + level
+    buttonCost.innerHTML = "¢" + currentBankCost
+    if (level >= 8) {
+      buttonCost.innerHTML = "MAX"
+    }
+
+    //button.classList.toggle("half");
+    button.animate({
+      transform: "scale(0.7) rotate(0deg)"
+    }, 35);
+    button.animate({
+      transform: "scale(1.001) rotate(0deg)"
+    }, 250);
+
+  } else if (cash < currentBankCost) {
+    let button = document.querySelector('#bankButton')//.querySelector('#49')
+    let buttonCost = button.querySelector('#bankCost')
+
+    var duration = 100
+
+    //text.style.background = "linear-gradient(180deg, rgb(253, 179, 7), rgb(244, 211, 101))"
+    buttonCost.style.visibility = (buttonCost.style.visibility == 'hidden' ? '' : 'hidden');
+    var executed = 0
+    //text.setAttribute('id', "blinking")
+    function blink() {
+        if (executed < 3) {
+            executed += 1;
+            buttonCost.style.visibility = (buttonCost.style.visibility == 'hidden' ? '' : 'hidden');
+        } 
     };
     //setInterval(blink, 1)
     setInterval(blink, duration)
